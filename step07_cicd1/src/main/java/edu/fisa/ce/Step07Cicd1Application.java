b8af72d9105bae15c9a6eeeff6258e1dbce7cdb9
SuGeunJee <jsgtwins1@gmail.com>
2025-03-24 10:35:33 +0900

    task1.project push

---
+++ step07_cicd1/src/main/java/edu/fisa/ce/Step07Cicd1Application.java
@@ -0,0 +1,13 @@
+package edu.fisa.ce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Step07Cicd1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Step07Cicd1Application.class, args);
+	}
+
+}
